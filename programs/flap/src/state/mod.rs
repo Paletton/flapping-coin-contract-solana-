d844c3acf2b462abb0a6bbc454ea7d44f9030e1e
superkidhedpay <cryptomusha@gmail.com>
2025-01-09 20:58:39 +0300

    raffle

--- programs/flap/src/state/mod.rs
+++ programs/flap/src/state/mod.rs
@@ -1,9 +1,9 @@
 pub mod app_stats;
 pub mod player;
 pub mod game;
-pub mod random_raffle;
+pub mod raffle;
 
 pub use app_stats::*;
 pub use player::*;
 pub use game::*;
-pub use random_raffle::*;
+pub use raffle::*;
